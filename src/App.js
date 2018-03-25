0399ecdde529f3cf273dae33dc10ad19331b7c73
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-03-25 20:10:56 +0700

    simple redux app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+        {
+          this.props.blogs.map((blog, index) =>
+            <div key={index}>
+              <div>Title: {blog.title}</div>
+              <div>Author: {blog.author}</div>
+            </div>
+          )
+        }
+        </div>
     );
   }
 }
+const mapStateToProps = (state) => {
+  return { blogs: state };
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+
+const initialState = [
+  {
+    title: 'My first blog',
+    author: 'Chatpong Suteesuksatporn'
+  }
+]
+
+const reducer = (state = initialState, action) => {
+  return state;
+}
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
